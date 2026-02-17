9184c3d4003ee517b57799edeb094bafe6d25e4b
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2026-02-17 23:55:35 +0530

    add auth middleware

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -8,26 +8,6 @@ const imagekit = new ImageKit({
 })
 
 async function createPostController(req, res) {
-    console.log(req.body, req.file)
-
-    const token = req.cookies.token;
-
-    if(!token) {
-        return res.status(401).json({
-            message: "Token not provided, Unauthorized access"
-        })
-    }
-
-    let decoded = null;
-
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (error) {
-        return res.status(401).json({
-            message: "user not authorized"
-        })
-    }
-
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer), 'file'),
         fileName: "Test",
@@ -37,7 +17,7 @@ async function createPostController(req, res) {
     const post = await postModel.create({
         caption: req.body.caption,
         imgUrl: file.url,
-        user: decoded.id
+        user: req.user.id
     })
 
     res.status(201).json({
@@ -47,25 +27,8 @@ async function createPostController(req, res) {
 }
 
 async function getPostController(req, res) {
-    const token = req.cookies.token;
-
-    if(!token) {
-        return res.status(401).json({
-            message: "unauthorized access"
-        })
-    }
-
-    let decoded;
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (error) {
-        return res.status(401).json({
-            message: "Token invalid"
-        })
-    }
-
     const posts = await postModel.find({
-        user: decoded.id
+        user: req.user.id
     })
 
     res.status(200).json({
@@ -76,24 +39,7 @@ async function getPostController(req, res) {
 }
 
 async function getPostDetailsController(req, res) {
-    const token = req.cookies.token;
-
-    if(!token) {
-        return res.status(401).json({
-            message: "UnAuthorized access"
-        })
-    }
-
-    let decoded;
-    try {
-        decoded = jwt.verify(token, process.env.JWT_SECRET)
-    } catch (error) {
-        return res.status(401).json({
-            message: "Invalid token"
-        })
-    }
-
-    const userId = decoded.id
+    const userId = req.user.id
     const postId = req.params.postId
 
     const post = await postModel.findById(postId)
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -6,10 +6,11 @@ const postRouter = express.Router()
 
 const storage = multer.memoryStorage()
 const upload = multer({ storage: storage})
+const identifyUser = require("../middlewares/auth.middleware")
 
-postRouter.post("/", upload.single("image"),postController.createPostController)
+postRouter.post("/", upload.single("image"), identifyUser, postController.createPostController)
 
-postRouter.get("/", postController.getPostController)
+postRouter.get("/", identifyUser, postController.getPostController)
 
-postRouter.get("/details/:postId", postController.getPostDetailsController)
+postRouter.get("/details/:postId", identifyUser, postController.getPostDetailsController)
 module.exports = postRouter
